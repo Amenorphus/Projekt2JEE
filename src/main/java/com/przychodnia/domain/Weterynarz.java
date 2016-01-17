134053af0b64018b1cb19546100e0f08b450f328
Amenorphus <robert779797@gmail.com>
2016-01-17 18:27:33 +0100

    Weterynarz

---
+++ src/main/java/com/przychodnia/domain/Weterynarz.java
@@ -0,0 +1,55 @@
+package com.przychodnia.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+
+public class Weterynarz 
+{
+	private Long id;
+	private String imie;
+	private String nazwisko;
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private List<Zwierze> zwierzes;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+	public String getNazwisko() {
+		return nazwisko;
+	}
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+	public List<Zwierze> getZwierzes() {
+		return zwierzes;
+	}
+	public void setZwierzes(List<Zwierze> zwierzes) {
+		this.zwierzes = zwierzes;
+	}
+	
+
+}
