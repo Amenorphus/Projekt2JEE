ad0eac650bb5bbda7c9322e806aa00dc85841c7a
Amenorphus <robert779797@gmail.com>
2016-01-17 15:16:11 +0100

    Zwierze

---
+++ src/main/java/com/przychodnia/domain/Zwierze.java
@@ -0,0 +1,61 @@
+package com.przychodnia.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+
+@Entity
+@NamedQueries({ 
+	@NamedQuery(name = "zwierze.all", query = "Select z from Zwierze z")
+})
+
+public class Zwierze 
+{
+
+	private Long id;
+	private String imie;
+	private String gatunek;
+	private Date dataur;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+	public String getGatunek() {
+		return gatunek;
+	}
+	public void setGatunek(String gatunek) {
+		this.gatunek = gatunek;
+	}
+	public Date getDataur() {
+		return dataur;
+	}
+	public void setDataur(Date dataur) {
+		this.dataur = dataur;
+	}
+	
+	
+}
