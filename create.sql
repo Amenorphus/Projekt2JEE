82a1575f688918f68ea48689054f158d71daf644
Amenorphus <robert779797@gmail.com>
2016-01-18 10:24:55 +0100

    create

---
+++ create.sql
@@ -0,0 +1,14 @@
+create table Zwierze
+(
+id INTEGER NOT NULL,
+imie varchar(40) not null,
+gatunek varchar(40) not null,
+dataur Date not null
+);
+
+create table Weterynarz
+(
+id INTEGER NOT NULL,
+imie varchar(40) not null,
+nazwisko varchar(40) not null,
+);
