fb1c5746f63cf56922f66b2a91936672519f0992
Amenorphus <robert779797@gmail.com>
2016-01-18 07:19:59 +0100

    jaxrsconfig

---
+++ src/main/java/com/przychodnia/api/JAXRSConfiguration.java
@@ -0,0 +1,8 @@
+package com.przychodnia.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+}
